2be6ef09deb605ab7222144e3816cf818096bfa9
Tan Yi Guan <tanyg1@outlook.sg>
2022-09-08 00:04:23 +0800

    update a3_zone_aware.sh

--- demo/a3/a3_zone_aware.sh
+++ demo/a3/a3_zone_aware.sh
@@ -1,5 +1,6 @@
 # apply zone aware backend manifest
 printf "deplying zone aware backend"
 kubectl apply -f k8s/manifests/backend-deployment-zone-aware.yml
+kubectl get nodes -L topology.kubernetes.io/zone
 kubectl wait --for=condition=ready pod -l app=backend-zone-aware --timeout=60s
 kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
